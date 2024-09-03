a5abb7131dd30fe99e34cd2685741037267a48b3
paurakhsharma <paurakh011@gmail.com>
2024-09-03 13:26:46 +0545

    refactor: Export video apis

--- lib/pod_player.dart
+++ lib/pod_player.dart
@@ -12,4 +12,5 @@ export 'src/models/pod_progress_bar_config.dart';
 export 'src/models/vimeo_models.dart';
 export 'src/pod_player.dart';
 export 'src/utils/enums.dart';
+export 'src/utils/video_apis.dart';
 export 'src/widgets/pod_progress_bar.dart';
